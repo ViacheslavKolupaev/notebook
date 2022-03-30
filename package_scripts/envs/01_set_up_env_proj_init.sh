a52fd35e48d6ee9df625305bf6ef11810ce7c775
vkolupaev <v.s.kolupaev@gmail.com>
2022-03-30 19:32:09 +0500

    Refactor 01_set_up_env_proj_init.sh

--- package_scripts/envs/01_set_up_env_proj_init.sh
+++ package_scripts/envs/01_set_up_env_proj_init.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #
-# Install project initialization dependencies in the virtual environment (venv).
-# Generate the first version of the "<platform>_<py_ver>_requirements.txt" file.
+# Install the application's dependencies into the virtual environment (venv).
+#
+# Files with incoming project dependency requirements:
+#  - "requirements/in/00_proj_init.in"
+#  - "requirements/in/01_app.in"
+#
+# Generated project dependency file: "01_app_requirements_<os_type>_py<python_version>.txt"
 #
 # Copyright 2022 Viacheslav Kolupaev, https://viacheslavkolupaev.ru/
 #
@@ -12,7 +17,7 @@
 #  Text message.
 #######################################
 function log_to_stdout() {
-    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&1
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&1
 }
 
 #######################################
@@ -21,7 +26,7 @@ function log_to_stdout() {
 #  Text message.
 #######################################
 function log_to_stderr() {
-    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
 }
 
 #######################################
@@ -86,6 +91,7 @@ function detect_os_type() {
 
 #######################################
 # Install updated versions of initialization dependencies from "00_proj_init.in" into venv.
+# For the script to work further, you must first install the "pip-tools" package.
 # Globals:
 #   pycharm_project_folder
 #   venv_name
@@ -97,25 +103,27 @@ function detect_os_type() {
 #   0 if there are no errors, non-zero on error.
 #######################################
 function install_upgrade_proj_init_dependencies() {
-    log_to_stdout 'Installing project initialization dependencies...'
-    log_to_stdout '--------------------------------------------------'
+  log_to_stdout 'Installing project initialization dependencies...'
+  log_to_stdout '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
 
-    if ! ${pycharm_project_folder}/${venv_name}/${venv_scripts_dir}/python -m pip install --upgrade \
-      --requirement ${pycharm_project_folder}/requirements/in/00_proj_init.in; then
-      log_to_stderr 'Error installing project initialization dependencies. Exit.'
-      exit 1
-    else
-      log_to_stdout '--------------------------------------------------'
-      log_to_stdout 'Project initialization dependencies installed successfully.'
-    fi
+  if ! ${pycharm_project_folder}/${venv_name}/${venv_scripts_dir}/python -m pip install --upgrade \
+    --requirement ${pycharm_project_folder}/requirements/in/00_proj_init.in; then
+    log_to_stderr 'Error installing project initialization dependencies. Exit.'
+    exit 1
+  else
+    log_to_stdout '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+    log_to_stdout 'Project initialization dependencies installed successfully.'
+  fi
 }
 
 #######################################
-# Compile "<platform>_<py_ver>_requirements.txt" from dependencies specified in "00_proj_init.in"
+# Compile "<req_file_name>_requirements_<os_type>_py<python_version>.txt".
+# Compilation is based on the file "<req_file_name>.in".
 # Globals:
 #   os_type
 #   pycharm_project_folder
 #   python_version
+#   req_file_name
 # Arguments:
 #  None
 #   Writes progress messages to stdout and error messages to stderr.
@@ -123,25 +131,29 @@ function install_upgrade_proj_init_dependencies() {
 #   0 if there are no errors, non-zero on error.
 #######################################
 function compile_requirements_file() {
-    log_to_stdout 'Compiling the resulting project dependency file...'
-    log_to_stdout '--------------------------------------------------'
+  local req_file_full_path
+  req_file_full_path="${pycharm_project_folder}/${req_file_name}_requirements_${os_type}_py${python_version}.txt"
+  readonly req_file_full_path
 
-    if ! pip-compile ${pycharm_project_folder}/requirements/in/00_proj_init.in \
-      --output-file=- >${pycharm_project_folder}/${os_type}_py${python_version}_requirements.txt; then
-      log_to_stderr 'Error compiling resulting project dependency file. Exit.'
-      exit 1
-    else
-      log_to_stdout '--------------------------------------------------'
-      log_to_stdout 'The resulting project dependency file was successfully compiled.'
-    fi
+  log_to_stdout "Compiling the resulting project dependency file: ${req_file_full_path}"
+  log_to_stdout '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+  if ! pip-compile ${pycharm_project_folder}/requirements/in/${req_file_name}.in \
+    --output-file=- >${req_file_full_path}; then
+    log_to_stderr 'Error compiling resulting project dependency file. Exit.'
+    exit 1
+  else
+    log_to_stdout '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+    log_to_stdout "The resulting project dependency file was successfully compiled: ${req_file_full_path}"
+  fi
 }
 
 #######################################
-# Synchronize project dependencies in venv with file "<platform>_<py_ver>_requirements.txt".
+# Synchronize project dependencies in venv with file "<req_file_name>_requirements_<os_type>_py<python_version>.txt".
 # Globals:
 #   os_type
 #   pycharm_project_folder
 #   python_version
+#   req_file_name
 # Arguments:
 #  None
 # Outputs:
@@ -150,16 +162,20 @@ function compile_requirements_file() {
 #   0 if there are no errors, non-zero on error.
 #######################################
 function sync_dependencies() {
-    log_to_stdout 'Project Dependency Synchronization...'
-    log_to_stdout '--------------------------------------------------'
+  local req_file_full_path
+  req_file_full_path="${pycharm_project_folder}/${req_file_name}_requirements_${os_type}_py${python_version}.txt"
+  readonly req_file_full_path
 
-    if ! pip-sync ${pycharm_project_folder}/${os_type}_py${python_version}_requirements.txt; then
-      log_to_stderr 'Error syncing project dependencies. Exit.'
-      exit 1
-    else
-      log_to_stdout '--------------------------------------------------'
-      log_to_stdout 'Project dependencies synced successfully.'
-    fi
+  log_to_stdout "Synchronizing the project's venv with the generated dependency file: ${req_file_full_path}"
+  log_to_stdout '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+
+  if ! pip-sync ${req_file_full_path}; then
+    log_to_stderr 'Error syncing project dependencies. Exit.'
+    exit 1
+  else
+    log_to_stdout '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+    log_to_stdout "The project venv was successfully synchronized with the dependency file: ${req_file_full_path}"
+  fi
 }
 
 #######################################
@@ -189,6 +205,10 @@ function main() {
   pycharm_project_folder="${HOME}/PycharmProjects/${project_name}"
   readonly pycharm_project_folder
 
+  local req_file_name
+  req_file_name="01_app"
+  readonly req_file_name
+
   local os_type
   os_type='unknown'
 
@@ -196,7 +216,7 @@ function main() {
   venv_scripts_dir='unknown'
 
 
-  log_to_stdout "${script_basename}: START"
+  log_to_stdout "${script_basename}: START SCRIPT EXECUTION"
 
   # Execution of nested functions.
   detect_os_type "$@"
@@ -204,7 +224,7 @@ function main() {
   compile_requirements_file "$@"
   sync_dependencies "$@"
 
-  log_to_stdout "${script_basename}: END"
+  log_to_stdout "${script_basename}: END OF SCRIPT EXECUTION"
 }
 
 main "$@"
