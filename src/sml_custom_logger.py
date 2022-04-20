376cedb95793023045ef4e49efce2e6d043be1ee
vkolupaev <v.s.kolupaev@gmail.com>
2022-04-20 18:55:59 +0500

    Update sml_custom_logger.py

--- src/sml_custom_logger.py
+++ src/sml_custom_logger.py
@@ -1,23 +1,24 @@
-#  Copyright (c) 2022. Viacheslav Kolupaev,  https://viacheslavkolupaev.ru/
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      https://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+"""Module for local testing of the logging module."""
 
-import logging
+#  Copyright (c) 2022. Viacheslav Kolupaev,  https://vkolupaev.com/
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 
 from src.boilerplate.custom_logger import LoggerWithMetadata
 from src.boilerplate.schemas.common import MetadataOpt  # type: ignore[import]
 
-_logger_w_metadata = LoggerWithMetadata(name=__name__)
+_module_logger = LoggerWithMetadata().get_module_logger(name=__name__)
 
 
 class LoggerTester(object):
@@ -26,19 +27,7 @@ class LoggerTester(object):
         self._metadata: MetadataOpt = metadata
 
     def log_error(self) -> None:
-        _logger_w_metadata.error('Some useful log entry.', extra=self._metadata)
-        # _logger_w_metadata.error('Some useful log entry.')
-
-
-    # def raise_log_exception(self) -> None:
-    #     try:
-    #         1/0
-    #     except ZeroDivisionError as exc:
-    #         _logger_w_metadata.exception(
-    #             '{exc_class_name} occurred.'.format(exc_class_name=exc.__class__.__name__),
-    #             extra=self._metadata,
-    #         )
-    #         raise exc
+        _module_logger.error('Some useful log entry.', extra=self._metadata.dict())
 
 
 def get_metadata() -> MetadataOpt:
@@ -53,7 +42,6 @@ def get_metadata() -> MetadataOpt:
 def main() -> None:
     metadata = get_metadata()
     lt = LoggerTester(metadata=metadata)
-    # lt.raise_log_exception()
 
     for i in range(0, 5):
         lt.log_error()
