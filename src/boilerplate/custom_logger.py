41668743a4b8966b23ffa6332607ea7aeef8c9ea
vkolupaev <v.s.kolupaev@gmail.com>
2022-04-21 18:26:49 +0500

    Refactor custom_logger.py

--- src/boilerplate/custom_logger.py
+++ src/boilerplate/custom_logger.py
@@ -13,35 +13,29 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-#
 
 import json
 import logging
 import sys
-from typing import Any, Optional, Union
+from typing import Any, Optional, Tuple, Union
 
 from typeguard import typechecked
 
-from src.boilerplate.schemas.common import MetadataMan, MetadataOpt, EnvState
 from src.boilerplate.config import config
+from src.boilerplate.schemas.common import EnvState, MetadataOpt  # type: ignore[import]
 
 
-class CustomAdapter(logging.LoggerAdapter):
+class CustomAdapter(logging.LoggerAdapter):  # type: ignore[type-arg]
+    """An adapter for loggers.
+
+    Makes it easier to specify contextual information in logging output.
+
+    Adds the keys and values from the "extra" keyword argument dictionary to the
+    beginning of the log messages.
     """
-    An adapter for loggers which makes it easier to specify contextual information
-    in logging output.
-
-    Prepend fields from MetadataOpt to the beginning of log messages.
-    """
-
-    def _prepend_dict_vals_to_str(self, prepend_str: str, extra: dict):
-        for key, val in extra.items():
-            prepend_str += '{key}: {val} | '.format(key=key, val=str(val))
-
-        return prepend_str
 
     @typechecked()
-    def process(self, msg: str, kwargs: Any) -> tuple[str, dict]:
+    def process(self, msg: str, kwargs: Any) -> tuple[str, Any]:
         """Process the logging message.
 
         Process the logging message and keyword arguments passed in to a logging call
@@ -56,7 +50,7 @@ class CustomAdapter(logging.LoggerAdapter):
         # `CustomAdapter(logger=some_logger, extra=some_dict)`
         if self.extra:
             if isinstance(self.extra, dict):
-                prepend_str = self._prepend_dict_vals_to_str(
+                prepend_str = self._prepend_extra_dict_to_str(
                     prepend_str=prepend_str,
                     extra=self.extra,
                 )
@@ -72,7 +66,7 @@ class CustomAdapter(logging.LoggerAdapter):
         # `_module_logger.debug(msg='some_message', extra=some_dict)`
         if 'extra' in kwargs and kwargs['extra']:
             if isinstance(kwargs['extra'], dict):
-                prepend_str = self._prepend_dict_vals_to_str(
+                prepend_str = self._prepend_extra_dict_to_str(
                     prepend_str=prepend_str,
                     extra=kwargs['extra'],
                 )
@@ -87,21 +81,31 @@ class CustomAdapter(logging.LoggerAdapter):
 
         return processed_msg, kwargs
 
-    # def make_error(self, msg: str, extra: Union[MetadataOpt, MetadataMan, dict], *args, **kwargs) -> None:
-    #     self.error(msg, *args, **kwargs)
+    @typechecked()
+    def _prepend_extra_dict_to_str(self, prepend_str: str, extra: dict[str, Any]) -> str:
+        for extra_key, extra_val in extra.items():  # noqa: WPS519
+            prepend_str += '{key}: {val} | '.format(key=extra_key, val=str(extra_val))
+
+        return prepend_str
 
 
-class LoggerWithMetadata(object):
+class LevelFilter(logging.Filter):
+
+    def __init__(self, low: int, high: int) -> None:
+        self._low = low
+        self._high = high
+        logging.Filter.__init__(self)
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        if self._low <= record.levelno <= self._high:
+            return True
+        return False
 
 
-    def __init__(self) -> None:
-        self._logger_name: Optional[str] = None
-        self._metadata: MetadataOpt = MetadataOpt()
-        self.extra: Optional[Union[MetadataOpt, dict]] = None
-        # self._default_stacklevel: int = 5
-        # self._logger_w_metadata: Union[CustomAdapter, logging.Logger] = self._get_logger()
+class CustomLogger(object):
 
-    def get_formatter(self) -> logging.Formatter:
+
+    def _get_root_logger_formatter(self) -> logging.Formatter:
 
         log_message_in_dict = {
             "timestamp": "%(asctime)s",
@@ -123,53 +127,16 @@ class LoggerWithMetadata(object):
 
         return log_message_formatter_in_str
 
-    @typechecked()
-    def get_root_logger(self) -> logging.Logger:
-        """Get root logger."""
-        root_logger: logging.Logger = logging.getLogger(name=None)
 
-        console_handler: logging.StreamHandler = logging.StreamHandler(sys.stdout)
-        error_handler: logging.StreamHandler = logging.StreamHandler(sys.stderr)
-
-        root_logger.logThreads = False
-        root_logger.logProcesses = False
-        logging.logMultiprocessing = False
-        root_logger.propagate = False
-
-        if config.APP_ENV_STATE in (EnvState.development, EnvState.staging):
-            root_logger.setLevel(logging.DEBUG)
-            console_handler.setLevel(logging.DEBUG)
-        else:
-            root_logger.setLevel(logging.INFO)
-            console_handler.setLevel(logging.INFO)
-
-            # See: https://docs.python.org/3/howto/logging.html#exceptions-raised-during-logging
-            root_logger.raiseExceptions = False
-
-        error_handler.setLevel(logging.ERROR)
-
-        console_handler.addFilter(LevelFilter(low=10, high=30))
-        error_handler.addFilter(LevelFilter(low=40, high=50))
-
-        formatter: logging.Formatter = self.get_formatter()
-
-        console_handler.setFormatter(formatter)
-        error_handler.setFormatter(formatter)
-
-        root_logger.addHandler(console_handler)
-        root_logger.addHandler(error_handler)
-
-        return root_logger
-
-    def get_module_logger(self, name: str) -> CustomAdapter:
-        """Get logger with prepared handlers and extra dict."""
-
-        logger: logging.Logger = logging.getLogger(name=name)
-
+    def _configure_logger(self, logger: logging.Logger) -> logging.Logger:
         logger.logThreads = False
         logger.logProcesses = False
-        logging.logMultiprocessing = False
-        logger.propagate = True
+        logger.logMultiprocessing = False
+
+        if logger.name == 'root':
+            logger.propagate = False
+        else:
+            logger.propagate = True
 
         if config.APP_ENV_STATE in (EnvState.development, EnvState.staging):
             logger.setLevel(logging.DEBUG)
@@ -179,89 +146,61 @@ class LoggerWithMetadata(object):
             # See: https://docs.python.org/3/howto/logging.html#exceptions-raised-during-logging
             logger.raiseExceptions = False
 
-        return CustomAdapter(logger=logger, extra={'key_1': 'val_1'})
+        return logger
 
 
-    # @typechecked()
-    # def log(
-    #     self,
-    #     level: int,
-    #     msg: str,
-    #     extra: Optional[Union[MetadataOpt, MetadataMan]] = None,
-    #     *args,
-    #     **kwargs,
-    # ) -> None:
-    #     stacklevel = kwargs.pop('stacklevel', self._default_stacklevel)
-    #     if extra:
-    #         self.extra = extra
-    #         self._logger_w_metadata.extra = extra.dict()
-    #     self._logger_w_metadata.log(level, msg, *args, stacklevel=stacklevel, **kwargs)
-    #
-    #
-    # @typechecked()
-    # def debug(self, msg: str, extra: Union[MetadataOpt, MetadataMan], *args, **kwargs) -> None:
-    #     stacklevel = kwargs.pop('stacklevel', self._default_stacklevel)
-    #     self.extra = extra
-    #     self._logger_w_metadata.extra = extra.dict()
-    #     self._logger_w_metadata.debug(msg, *args, stacklevel=stacklevel, **kwargs)
-    #
-    #
-    # @typechecked()
-    # def info(self, msg: str, extra: Optional[Union[MetadataOpt, MetadataMan]] = None, *args, **kwargs) -> None:
-    #     stacklevel = kwargs.pop('stacklevel', self._default_stacklevel)
-    #     if extra:
-    #         self.extra = extra
-    #         self._logger_w_metadata.extra = extra.dict()
-    #     self._logger_w_metadata.info(msg, *args, stacklevel=stacklevel, **kwargs)
-    #
-    #
-    # @typechecked()
-    # def warning(self, msg: str, extra: Union[MetadataOpt, MetadataMan], *args, **kwargs) -> None:
-    #     stacklevel = kwargs.pop('stacklevel', self._default_stacklevel)
-    #     self.extra = extra
-    #     self._logger_w_metadata.extra = extra.dict()
-    #     self._logger_w_metadata.warning(msg, *args, stacklevel=stacklevel, **kwargs)
-    #
-    #
-    # @typechecked()
-    # def error(self, msg: str, extra: Union[MetadataOpt, MetadataMan], *args, **kwargs) -> None:
-    #     stacklevel = kwargs.pop('stacklevel', self._default_stacklevel)
-    #     self.extra = extra
-    #     self._logger_w_metadata.extra = extra.dict()
-    #     self._logger_w_metadata.error(msg, *args, stacklevel=stacklevel, **kwargs)
-    #
-    #
-    # @typechecked()
-    # def critical(self, msg: str, extra: Union[MetadataOpt, MetadataMan], *args, **kwargs) -> None:
-    #     stacklevel = kwargs.pop('stacklevel', self._default_stacklevel)
-    #     self.extra = extra
-    #     self._logger_w_metadata.extra = extra.dict()
-    #     self._logger_w_metadata.critical(msg, *args, stacklevel=stacklevel, **kwargs)
-    #
-    #
-    # @typechecked()
-    # def exception(
-    #         self,
-    #         msg: str,
-    #         extra: Union[MetadataOpt, MetadataMan],
-    #         exc_info: Union[bool, Exception] = True,
-    #         *args,
-    #         **kwargs
-    # ) ->  None:
-    #     stacklevel = kwargs.pop('stacklevel', self._default_stacklevel)
-    #     self.extra = extra
-    #     self._logger_w_metadata.extra = extra.dict()
-    #     self._logger_w_metadata.exception(msg, exc_info=exc_info, stacklevel=stacklevel, *args, **kwargs)
+    def _get_root_logger_console_handler(
+        self,
+        formatter: logging.Formatter,
+    ) -> logging.StreamHandler:  # type: ignore[type-arg]
+        console_handler = logging.StreamHandler(stream=sys.stdout)
+
+        if config.APP_ENV_STATE in (EnvState.development, EnvState.staging):
+            console_handler.setLevel(logging.DEBUG)
+        else:
+            console_handler.setLevel(logging.INFO)
+
+        console_handler.addFilter(LevelFilter(low=10, high=30))
+        console_handler.setFormatter(formatter)
+
+        return console_handler
 
 
-class LevelFilter(logging.Filter):
+    def _get_root_logger_error_handler(
+        self,
+        formatter: logging.Formatter,
+    ) -> logging.StreamHandler:  # type: ignore[type-arg]
+        error_handler = logging.StreamHandler(stream=sys.stderr)
+        error_handler.setLevel(logging.ERROR)
+        error_handler.addFilter(LevelFilter(low=40, high=50))
+        error_handler.setFormatter(formatter)
 
-    def __init__(self, low: int, high: int) -> None:
-        self._low = low
-        self._high = high
-        logging.Filter.__init__(self)
+        return error_handler
 
-    def filter(self, record) -> bool:
-        if self._low <= record.levelno <= self._high:
-            return True
-        return False
+
+    def get_root_logger(self) -> logging.Logger:
+        """Get root logger."""
+        root_logger = logging.getLogger(name=None)
+        root_logger = self._configure_logger(logger=root_logger)
+
+        formatter = self._get_root_logger_formatter()
+        console_handler = self._get_root_logger_console_handler(formatter=formatter)
+        error_handler = self._get_root_logger_error_handler(formatter=formatter)
+        root_logger.addHandler(console_handler)
+        root_logger.addHandler(error_handler)
+
+        return root_logger
+
+
+    @typechecked()
+    def get_module_logger(
+        self,
+        name: str,
+        module_extra: Optional[dict[str, Any]] = None,
+    ) -> CustomAdapter:
+        """Get logger with prepared handlers and extra dict."""
+        module_logger = logging.getLogger(name=name)
+        module_logger = self._configure_logger(logger=module_logger)
+        adapter = CustomAdapter(logger=module_logger, extra=module_extra)
+
+        return adapter
