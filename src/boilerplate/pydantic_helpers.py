68fb60d2ea1137fdabc16209ca110260ac43684c
vkolupaev <v.s.kolupaev@gmail.com>
2022-05-04 19:16:43 +0500

    Update pydantic_helpers.py

--- src/boilerplate/pydantic_helpers.py
+++ src/boilerplate/pydantic_helpers.py
@@ -1,5 +1,19 @@
 """The module contains helper tools for validating and transforming Pydantic model data."""
 
+#  Copyright (c) 2022. Viacheslav Kolupaev, https://vkolupaev.com/
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 from typing import Any, Callable, Optional
 
 import orjson
@@ -22,8 +36,10 @@ def replace_empty_values_to_none(checked_value: Any) -> Any:
 def orjson_dumps(processed_value: Any, *, default: Optional[Callable[[Any], Any]]) -> Any:
     """Make `orjson` (de)serialisation.
 
+    This is a custom function for encoding JSON; see custom JSON (
+    de)serialisation: https://pydantic-docs.helpmanual.io/usage/exporting_models/#custom-json-deserialisation
+
     `orjson` takes care of `datetime` encoding natively.
-    Documentation: https://pydantic-docs.helpmanual.io/usage/exporting_models/#custom-json-deserialisation
     """
     # orjson.dumps returns bytes, to match standard json.dumps we need to decode
     return orjson.dumps(processed_value, default=default).decode()
