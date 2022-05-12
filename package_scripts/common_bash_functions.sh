376f465509c55d95bbedf9bd7cc6cb8b2e7f9a77
Viacheslav Kolupaev <v.s.kolupaev@gmail.com>
2022-05-12 18:27:48 +0500

    Add `sync_project_dependencies` function

--- package_scripts/common_bash_functions.sh
+++ package_scripts/common_bash_functions.sh
@@ -48,6 +48,7 @@ function log_to_stderr() {
 function docker_container_stop() {
   local container_id_or_name
   container_id_or_name=$1
+  readonly container_id_or_name
 
   log_to_stdout "Stopping the '${container_id_or_name}' container..."
   if ! docker stop "${container_id_or_name}"; then
@@ -66,6 +67,7 @@ function docker_container_stop() {
 function docker_container_remove() {
   local container_id_or_name
   container_id_or_name=$1
+  readonly container_id_or_name
 
   log_to_stdout "Removing the '${container_id_or_name}' container..."
   if ! docker rm "${container_id_or_name}"; then
@@ -84,6 +86,7 @@ function docker_container_remove() {
 function docker_image_remove() {
   local image_id_or_name
   image_id_or_name=$1
+  readonly image_id_or_name
 
   log_to_stdout "Removing the '${image_id_or_name}' image..."
   if ! docker rmi --force "${image_id_or_name}"; then
@@ -92,4 +95,50 @@ function docker_image_remove() {
   else
     log_to_stdout "Image '${image_id_or_name}' removed successfully. Continue."
   fi
+}
+
+#######################################
+# Synchronize project dependencies with the specified compiled dependency file(s).
+# Arguments:
+#   The full path to the compiled dependency file, with which to sync. Required.
+#   Path to the root of the project. Optional. If specified, will additionally sync with `01_app_requirements.txt`.
+#######################################
+function sync_project_dependencies() {
+  log_to_stdout "Synchronizing project dependencies with the specified requirements file(s)..."
+  log_to_stdout '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+
+  if [ -z "$1" ] ; then
+    log_to_stderr "Argument 'req_compiled_file_full_path' was not specified in the function call. Exit."
+    exit 1
+  else
+    local req_compiled_file_full_path
+    req_compiled_file_full_path=$1
+    readonly req_compiled_file_full_path
+    log_to_stdout "File 1: ${req_compiled_file_full_path}"
+  fi
+
+  if [ -n "$2" ] ; then
+    local project_root
+    project_root=$2
+    readonly project_root
+    log_to_stdout "File 2: ${project_root}/requirements/compiled/01_app_requirements.txt"
+
+    if ! pip-sync \
+        "${project_root}/requirements/compiled/01_app_requirements.txt" \
+        "${req_compiled_file_full_path}"; then
+      log_to_stderr 'Error syncing project dependencies. Exit.'
+      exit 1
+    fi
+
+  else
+
+    if ! pip-sync "${req_compiled_file_full_path}"; then
+      log_to_stderr 'Error syncing project dependencies. Exit.'
+      exit 1
+    fi
+
+  fi
+
+  log_to_stdout '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+  log_to_stdout "The project's venv dependencies were successfully synced to the specified requirements file(s)."
 }
