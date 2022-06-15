eeba6c737174cd2c4ba23f20cd67f03ab079f0fe
Viacheslav Kolupaev <v.s.kolupaev@gmail.com>
2022-06-15 18:01:35 +0500

    Update copyright notice

--- common_bash_functions.sh
+++ common_bash_functions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#
+##########################################################################################
 # Copyright (c) 2022. Viacheslav Kolupaev, https://vkolupaev.com/
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
-# You may obtain a copy of the License atf
+# You may obtain a copy of the License at
 #
 #   https://www.apache.org/licenses/LICENSE-2.0
 #
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+##########################################################################################
 
 ##########################################################################################
 # The script provides common bash functions.
