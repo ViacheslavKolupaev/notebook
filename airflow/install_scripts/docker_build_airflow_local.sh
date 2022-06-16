07b1e4ed79c359e0cdeb0e35b65f9fa4babb0a1b
Viacheslav Kolupaev <v.s.kolupaev@gmail.com>
2022-06-16 11:24:11 +0500

    Update copyright notice and file docstring

--- airflow/install_scripts/docker_build_airflow_local.sh
+++ airflow/install_scripts/docker_build_airflow_local.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+##########################################################################################
 # Copyright (c) 2022. Viacheslav Kolupaev, https://vkolupaev.com/
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+##########################################################################################
 
 ##########################################################################################
 # The script will build a Docker image with standalone Apache Airflow.
