6d390327f705f80115709e6558d3f50979018431
Viacheslav Kolupaev <v.s.kolupaev@gmail.com>
2022-06-27 18:53:28 +0500

    Fix some mypy and flake8 issues

--- src/boilerplate/routers/admin_controller.py
+++ src/boilerplate/routers/admin_controller.py
@@ -25,7 +25,6 @@ from fastapi import APIRouter, Depends, status
 from src.boilerplate.config import DevelopmentConfig, ProductionConfig, StagingConfig, config
 from src.boilerplate.custom_logger import CustomLogger
 from src.boilerplate.dependencies import is_media_type_application_json, is_request_has_correct_http_bearer_token
-from src.boilerplate.schemas.common import MetadataOpt  # type: ignore[import]
 
 _module_logger = CustomLogger().get_module_logger(
     name=__name__,
@@ -33,14 +32,14 @@ _module_logger = CustomLogger().get_module_logger(
 )
 
 router = APIRouter(
-    prefix=f"/api/{config.APP_API_VERSION}/admin",
+    prefix='/api/{app_api_version}/admin'.format(app_api_version=config.APP_API_VERSION),
     tags=['admin-controller'],
     responses={
         status.HTTP_401_UNAUTHORIZED: {'description': 'Unauthorized'},
         status.HTTP_403_FORBIDDEN: {'description': 'Forbidden'},
         status.HTTP_404_NOT_FOUND: {'description': 'Not found'},
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE: {
-            'description': 'Unsupported MIME type in header `Accept` or not provided'
+            'description': 'Unsupported MIME type in header `Accept` or not provided',
         },
     },
     dependencies=[
@@ -51,18 +50,21 @@ router = APIRouter(
 
 
 @router.get(
-    path="/app-config",
-    response_model=Union[StagingConfig, ProductionConfig, DevelopmentConfig],
+    path='/app-config',
+    # To perform the field limiting and serialization.
+    # Docs: https://fastapi.tiangolo.com/tutorial/response-model/
+    response_model=Union[DevelopmentConfig, StagingConfig, ProductionConfig],  # type: ignore[arg-type]
+    response_model_exclude={},
     status_code=status.HTTP_200_OK,
     responses={
         status.HTTP_200_OK: {
             'description': 'Requested data returned.',
-            'model': Union[StagingConfig, ProductionConfig, DevelopmentConfig],
+            'model': Union[DevelopmentConfig, StagingConfig, ProductionConfig],
         },
     },
-    summary="Get the application config depending on the environment."
+    summary='Get the application config depending on the environment.',
 )
-async def get_app_config() -> Union[StagingConfig, ProductionConfig, DevelopmentConfig]:
+async def get_app_config() -> Union[DevelopmentConfig, StagingConfig, ProductionConfig]:
     """Get the application config.
 
     Depending on the environment, the method will return one of the following models:
@@ -75,16 +77,16 @@ async def get_app_config() -> Union[StagingConfig, ProductionConfig, Development
     """
     return config
 
-@router.get("/check-sentry")
-async def send_event_division_by_zero():
+
+@router.get('/check-sentry')
+async def trigger_sending_an_exception_to_sentry() -> dict[str, str]:
+    """Initiate sending an exception to Sentry.
+
+    A divide-by-zero attempt is used to initiate.
+    """
     try:
-        1 / 0
-    except ZeroDivisionError as e:
-        _module_logger.exception("ZeroDivisionError occurred: '%s'.", e)  # will mark the exception
+        1 / 0  # noqa: WPS428, WPS344
+    except ZeroDivisionError:
+        _module_logger.exception(msg='A `ZeroDivisionError` has occurred.')
 
-    return {"message": "An error message has been sent to Sentry."}
-
-
-@router.get("/check-http-bearer-auth")
-async def check_http_bearer_auth():
-    return {"HTTP Bearer Auth": "OK"}
+    return {'message': 'An error message has been sent to Sentry.'}
