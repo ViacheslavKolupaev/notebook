45b7ee54653e0b9cc05fbb6e9094a257d4cb1388
Viacheslav Kolupaev <v.s.kolupaev@gmail.com>
2022-11-28 20:19:17 +0500

    build(00_docker_build_local.sh): add `python_image_tag` arg
    Remove `--pull` arg.

--- package_scripts/docker/00_docker_build_local.sh
+++ package_scripts/docker/00_docker_build_local.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-##########################################################################################
-# Copyright (c) 2022. Viacheslav Kolupaev, https://vkolupaev.com/
+# ########################################################################################
+#  Copyright (c) 2022 Viacheslav Kolupaev; author's website address:
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
-# file except in compliance with the License. You may obtain a copy of the License at
+#      https://vkolupaev.com/?utm_source=c&utm_medium=link&utm_campaign=notebook
 #
-#   https://www.apache.org/licenses/LICENSE-2.0
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
 #
-# Unless required by applicable law or agreed to in writing, software distributed under
-# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied. See the License for the specific language governing
-# permissions and limitations under the License.
-##########################################################################################
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# ########################################################################################
 
 ##########################################################################################
 # The script will build a Docker image of the `boilerplate` application.
@@ -27,7 +31,8 @@
 # If necessary, you need to replace the values of the variables in the `main()` function:
 # - `dockerfile_dir`;
 # - `docker_image_name`;
-# - `docker_image_tag`.
+# - `docker_image_tag`;
+# - `python_image_tag`.
 #
 # The script uses the helper functions from the `common_bash_functions.sh` file.
 ##########################################################################################
@@ -54,6 +59,7 @@ function import_library_of_common_bash_functions() {
 #  docker_image_name
 #  docker_image_tag
 #  dockerfile_dir
+#  python_image_tag
 #######################################
 function docker_build_boilerplate_image() {
   echo ''
@@ -62,47 +68,58 @@ function docker_build_boilerplate_image() {
 
   # Checking function arguments.
   if [ -z "$1" ] || [ "$1" = '' ] || [[ "$1" = *' '* ]] ; then
-    log_to_stderr "Argument 'docker_image_name' was not specified in the function call. Exit."
-    exit 1
-  else
-    local docker_image_name
-    docker_image_name=$1
-    readonly docker_image_name
-    log_to_stdout "Argument 'docker_image_name' = ${docker_image_name}"
-  fi
-
-  if [ -z "$2" ] || [ "$2" = '' ] || [[ "$2" = *' '* ]] ; then
-    log_to_stderr "Argument 'docker_image_tag' was not specified in the function call. Exit."
-    exit 1
-  else
-    local docker_image_tag
-    docker_image_tag=$2
-    readonly docker_image_tag
-    log_to_stdout "Argument 'docker_image_tag' = ${docker_image_tag}"
-  fi
-
-  if [ -z "$3" ] || [ "$3" = '' ] || [[ "$3" = *' '* ]] ; then
     log_to_stderr "Argument 'dockerfile_dir' was not specified in the function call. Exit."
     exit 1
   else
     local dockerfile_dir
-    dockerfile_dir=$3
+    dockerfile_dir=$1
     readonly dockerfile_dir
-    log_to_stdout "Argument 'dockerfile_dir' = ${dockerfile_dir}"
+    log_to_stdout "Argument 'dockerfile_dir' = ${dockerfile_dir}" 'Y'
+  fi
+
+  if [ -z "$2" ] || [ "$2" = '' ] || [[ "$2" = *' '* ]] ; then
+    log_to_stderr "Argument 'docker_image_name' was not specified in the function call. Exit."
+    exit 1
+  else
+    local docker_image_name
+    docker_image_name=$2
+    readonly docker_image_name
+    log_to_stdout "Argument 'docker_image_name' = ${docker_image_name}" 'Y'
+  fi
+
+  if [ -z "$3" ] || [ "$3" = '' ] || [[ "$3" = *' '* ]] ; then
+    log_to_stderr "Argument 'docker_image_tag' was not specified in the function call. Exit."
+    exit 1
+  else
+    local docker_image_tag
+    docker_image_tag=$3
+    readonly docker_image_tag
+    log_to_stdout "Argument 'docker_image_tag' = ${docker_image_tag}" 'Y'
+  fi
+
+  if [ -z "$4" ] || [ "$4" = '' ] || [[ "$4" = *' '* ]] ; then
+    log_to_stderr "Argument 'python_image_tag' was not specified in the function call. Exit."
+    exit 1
+  else
+    local python_image_tag
+    python_image_tag=$4
+    readonly python_image_tag
+    log_to_stdout "Argument 'python_image_tag' = ${python_image_tag}" 'Y'
   fi
 
   # Get the short SHA of the current Git revision.
   local git_rev_short_sha
   git_rev_short_sha="$(git -C ${dockerfile_dir} rev-parse --short HEAD)"
-  log_to_stdout "git_rev_short_sha: ${git_rev_short_sha}"
+  readonly git_rev_short_sha
+  log_to_stdout "git_rev_short_sha: ${git_rev_short_sha}" 'Y'
 
   # Building a Docker image.
   # See about `DOCKER_BUILDKIT`: https://github.com/moby/moby/issues/34151#issuecomment-739018493
   # See about `DOCKER_SCAN_SUGGEST`: https://github.com/docker/scan-cli-plugin/issues/149#issuecomment-823969364
   if ! DOCKER_BUILDKIT=1 DOCKER_SCAN_SUGGEST=false docker build \
        --progress=plain \
-       --pull \
        --file "${dockerfile_dir}/Dockerfile" \
+       --build-arg PYTHON_IMAGE_TAG="${python_image_tag}" \
        --build-arg VCS_REF="${git_rev_short_sha}" \
        --build-arg APP_NAME="${docker_image_name}" \
        --tag "${docker_image_name}:${docker_image_tag}" \
@@ -132,8 +149,13 @@ function main() {
   local docker_image_name
   readonly docker_image_name='boilerplate'  # change if necessary
 
+  # Get the current branch tag in Git.
   local docker_image_tag
-  readonly docker_image_tag='latest'  # change if necessary
+  docker_image_tag="$(git -C ${dockerfile_dir} describe --tags --abbrev=0)"
+  readonly docker_image_tag
+
+  local python_image_tag
+  readonly python_image_tag='3.10.6-slim'  # change if necessary
 
   # 2. Import the library of common bash functions.
   import_library_of_common_bash_functions
@@ -148,9 +170,11 @@ function main() {
     "${docker_image_tag}"
 
   docker_build_boilerplate_image \
+    "${dockerfile_dir}" \
     "${docker_image_name}" \
     "${docker_image_tag}" \
-    "${dockerfile_dir}"
+    "${python_image_tag}"
+
 
   log_to_stdout 'END OF SCRIPT EXECUTION.' 'Bl'
 }
